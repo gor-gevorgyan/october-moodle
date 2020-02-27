28d34caf821e8db28c3985216c58733c9c57bb88
ss <gor92gevorgyan@gmail.com>
2020-02-27 13:24:51 +0400

    Simple MoodleCourses page with table

--- plugins/gor/moodle/components/Classes.php
+++ plugins/gor/moodle/components/Classes.php
@@ -2,9 +2,8 @@
 
 use Cms\Classes\ComponentBase;
 
-class CLasses extends ComponentBase
+class Classes extends ComponentBase
 {
-
     public function componentDetails()
     {
         return [
@@ -20,16 +19,31 @@ class CLasses extends ComponentBase
 
     public function init()
     {
+        $token = config('moodle.token'); // d39ac25b9f01ff86acd4bd34aa2cde9c
+        $domain_name = config('moodle.domain'); // https://test-as.moodlecloud.com
+        $function_name = 'core_course_get_courses';
+        $rest_format = 'json';
 
+        $server_url = $domain_name . '/webservice/rest/server.php' . '?wstoken=' . $token . '&wsfunction=' . $function_name . '&moodlewsrestformat=' . $rest_format;
+
+        try {
+            $courses_json = file_get_contents($server_url, true);
+        } catch (\Exception $e) {
+            $courses_json = '{}';
+        }
+
+        $courses = json_decode($courses_json, true);
+        $this->page['courses'] = $courses;
     }
 
     public function onRun()
     {
-   
+        //ss https://codepen.io/vasansr/pen/PZOJXr
+        $this->addCss('assets/css/table.css');
     }
 
     public function onRender()
     {
-
+        //
     }
 }
